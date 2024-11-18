fea3179ef91ff467d998c99a859981bd91d1cfab
rafaelgdgs <rafagsique@hotmail.com>
2024-11-18 18:51:25 -0300

    feat: fix script so no change on src file needed

--- run_and_write.sh
+++ run_and_write.sh
@@ -3,8 +3,8 @@
 echo "Choose the file: "
 read filename
 
-echo "j30/j30$filename.sm   $(python main.py --file ./data/data/j30/j30$filename.sm)" >>"./data/alg_result.txt"
-echo "j60/j60$filename.sm   $(python main.py --file ./data/data/j60/j60$filename.sm)" >>"./data/alg_result.txt"
-echo "j90/j90$filename.sm   $(python main.py --file ./data/data/j90/j90$filename.sm)" >>"./data/alg_result.txt"
-echo "j120/j120$filename.sm   $(python main.py --file ./data/data/j120/j120$filename.sm)" >>"./data/alg_result.txt"
+echo "j30/j30$filename.sm   $(python main.py --file ./data/data/j30/j30$filename.sm | awk -F'[(), ]+' '{print $(NF-1)}')" >>"./data/alg_result.txt"
+echo "j60/j60$filename.sm   $(python main.py --file ./data/data/j60/j60$filename.sm | awk -F'[(), ]+' '{print $(NF-1)}')" >>"./data/alg_result.txt"
+echo "j90/j90$filename.sm   $(python main.py --file ./data/data/j90/j90$filename.sm | awk -F'[(), ]+' '{print $(NF-1)}')" >>"./data/alg_result.txt"
+echo "j120/j120$filename.sm   $(python main.py --file ./data/data/j120/j120$filename.sm | awk -F'[(), ]+' '{print $(NF-1)}')" >>"./data/alg_result.txt"
 echo "" >>"./data/alg_result.txt"
